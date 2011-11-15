2323aeb1ad7c60ea6953b6318afe9cc1efeda1da
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2011-11-14 23:28:17 -0500

    basic reading from stdout/err working

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -5,6 +5,7 @@ from pyramid.view import view_config
 
 import logging
 from subprocess import Popen, PIPE
+import fcntl
 import uuid
 import os
 
@@ -49,6 +50,11 @@ error = lambda msg, **kwargs: response('error', msg=msg, **kwargs)
 def status(request):
     return success(jobs=dict((j, job(j)) for j in Jobs))
 
+def set_nonblocking(f):
+    fd = f.fileno()
+    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
 @view_config(route_name='jobs', renderer='json', request_method='PUT')
 def start_job(request):
     if 'name' not in request.POST:
@@ -68,19 +74,52 @@ def start_job(request):
 
     Jobs[uid] = Popen(path, stdin=PIPE, stdout=PIPE, stderr=PIPE, 
                       close_fds=True)
-
+    set_nonblocking(Jobs[uid].stdout)
+    set_nonblocking(Jobs[uid].stderr)
+    
     return success(uid=uid, name=name)
 
-@view_config(route_name='job', renderer='json', request_method='DELETE')
-def stop_job(request):
-    return None
+def validate_job_uid(func):
+    def _validate_job_uid(request):
+        
+        if 'uid' not in request.matchdict:
+            return error('No UID specified')
 
-@view_config(route_name='job', renderer='json', request_method='GET')
-def read_from_job(request):
-    return None
+        uid = request.matchdict['uid']
+        
+        if uid not in Jobs:
+            return error('Unknown UID')
+
+        return func(request, uid, Jobs[uid])
+
+    return _validate_job_uid
+
+@view_config(route_name='job', renderer='json', request_method='DELETE')
+@validate_job_uid
+def stop_job(request, uid, job):
+    if request.POST.get('kill') == '1':
+        job.kill()
+    else:
+        job.terminate()
+
+    return success()
+
+def read_from_job(request, uid, job, f, key):
+    return success(**{key: f.read()})
+
+@view_config(route_name='job-stdout', renderer='json', request_method='GET')
+@validate_job_uid
+def read_from_stdout(request, uid, job):
+    return read_from_job(request, uid, job, job.stdout, 'stdout')
+
+@view_config(route_name='job-stderr', renderer='json', request_method='GET')
+@validate_job_uid
+def read_from_stdout(request, uid, job):
+    return read_from_job(request, uid, job, job.stderr, 'stderr')
 
 @view_config(route_name='job', renderer='json', request_method='POST')
-def write_to_job(request):
+@validate_job_uid
+def write_to_job(request, uid, job):
     return None
 
 def main(global_config, **settings):
@@ -88,6 +127,8 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_route('jobs', '/jobs')
     config.add_route('job', '/jobs/{uid}')
+    config.add_route('job-stdout', '/jobs/{uid}/stdout')
+    config.add_route('job-stderr', '/jobs/{uid}/stderr')
 
     config.scan()
 
